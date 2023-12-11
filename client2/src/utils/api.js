a5c0356352fa92267a3a921e438a6b2cac1f9631
Xhunter <kushwah.kartikey14@upesalumni.upes.ac.in>
2023-12-11 11:11:13 +0530

    remove(redundancy): remove redundant & dead data

--- client2/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { BASE_URL } from "../content_option";
-
-const client = axios.create({
-  baseURL: BASE_URL ? BASE_URL : "http://localhost:8086",
-});
-
-export default client;
